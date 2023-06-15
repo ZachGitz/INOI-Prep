326cc1eee55523ba1f4e6b9e7fbae7308c91ecd5
Zach <82441973+ZachGitz@users.noreply.github.com>
2023-06-16 02:23:56 +0530

    Create ZCO-SmartPhone.cpp

---
+++ ZCO-SmartPhone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+
+int main() {
+	int n;
+	cin >> n;
+	int a[n],b[n];
+	for(int i =0 ; i<n;i++){
+	    cin >> a[i];
+	}
+      
+      
+    sort(a,a+n);
+	
+	for(int i=0;i<=n;i++){
+	   b[i] =a[i]*(n-i);
+	}
+	sort(b,b+n);
+	cout << b[n-1];
+	
+ return 0;
+}
