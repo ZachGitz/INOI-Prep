bb364ec328b3f6864ae3762e2d87f5535efd9e75
Zach <82441973+ZachGitz@users.noreply.github.com>
2023-06-20 03:04:06 +0530

    Billiards-CDQU5.cpp

--- Billiards-CDQU5.cpp
+++ Billiards-CDQU5.cpp
@@ -3,19 +3,19 @@ using namespace std;
 int n;
 int dp[1000009];
 int mod = 1000000009;
-int score(long n){
+int score(int n){
     if(dp[n]!=-1)
     return dp[n];
-    return dp[n]=(score(n-3)%mod+score(n-2)%mod)%mod;
+    return dp[n]=(score(n-3)+score(n-2))%mod;
 }
 int main() {
     int c;
+   
 	cin >> c;
-	
 	dp[1]=0;
 	dp[2]=1;
 	dp[3]=1;
-	for(int i=4;i<1000;i++)
+	for(int i=4;i<1000009;i++)
 	dp[i]=-1;
 	while(c--){
 	cin >> n;
