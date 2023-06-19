b42f9ab9da91bf5b55d84a375061abdda28f8c09
Zach <82441973+ZachGitz@users.noreply.github.com>
2023-06-20 04:07:09 +0530

    SUPW-ZCO14002.cpp

---
+++ SUPW-ZCO14002.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <algorithm>
+#include <bits/stdc++.h>
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    int days[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin >> days[i];
+    }
+    int dp[n];
+    dp[0]=days[0];
+    dp[1]=days[1];
+    dp[2]=days[2];
+    for(int i=3;i<n;i++){
+    dp[i]=min({dp[i-1],dp[i-2],dp[i-3]})+days[i];
+    }
+    cout << min({dp[n-1],dp[n-2],dp[n-3]});
+	return 0;
+}
